0a5485ba7373b3350601abec24e8dfe585e1ef6f
Phuc <phucdc99@gmail.com>
2025-05-24 20:22:46 +0700

    50

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.scss';
+import Header from './components/Header/Header';
+import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app-container">
-      Hello world
+      <div className='header-container'>
+        <Header />
+      </div>
+
+      <div className='main-container'>
+        <div className='sidenav-container'>
+
+        </div>
+        <div className='app-content'>
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 }
